5123764f3aa786084688beb5fe21257f55a532c8
Bastiangm23 <bastian.garcia.m@gmail.com>
2023-04-04 22:06:33 -0400

    hjkkj

---
+++ index.js
@@ -0,0 +1,17 @@
+const btn_anterior = document.getElementById("btn-anterior");
+const btn_siguiente = document.getElementById("btn-siguiente");
+
+const reviews_container = document.querySelector(".reviews-container");
+let movimiento = 0
+
+btn_siguiente.addEventListener("click", function(){
+    console.log("presionaste siguiente");
+    movimiento += -33.3
+    reviews_container.style.transform = `translateX(${movimiento}%)`;
+})
+
+btn_anterior.addEventListener("click", function(){
+    console.log("presionaste anterior");
+    movimiento += 33.3
+    reviews_container.style.transform = `translateX(${movimiento}%)`;
+})
